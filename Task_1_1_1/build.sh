10cf91f804407f9ca86f8786b2770f0a3a4ba882
Victoria Serova <serovaviktoria95@gmail.com>
2026-09-24 19:26:52 +0700

    Task_1_1_2

--- Task_1_1_1/build.sh
+++
@@ -1,4 +0,0 @@
-javac -d build/classes src/main/java/ru/nsu/vserova/task111/*.java
-javadoc -d build/docs src/main/java/ru/nsu/vserova/task111/*.java
-jar cf build/heapsort.jar -C build/classes .
-java -cp build/classes ru.nsu.vserova.task111.Main
